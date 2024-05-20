294cf5ad5d9f1fa3d9d5d6a2875655d3d120063b
juijsj <wbokgm@gmail.com>
2024-05-19 23:45:22 -0500

    cambio en dockerArreglotest

--- src/main/java/com/estivman/physics_proyect/physics_simulator/utils/MaterialJsonReader.java
+++ src/main/java/com/estivman/physics_proyect/physics_simulator/utils/MaterialJsonReader.java
@@ -12,23 +12,22 @@ import java.util.List;
 import com.estivman.physics_proyect.physics_simulator.models.Material;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
 
 public class MaterialJsonReader {
 
     public List<Material> loadSolidMaterials() {
         Gson gson = new Gson();
-        Type listType = new TypeToken<List<Material>>() {
-        }.getType();
+        Type listType = new TypeToken<List<Material>>() {}.getType();
         List<Material> materials = new ArrayList<>();
-        try {
-            JsonReader reader = new JsonReader(new FileReader("src/main/resources/solid-materials.json"));
+        try (InputStream inputStream = getClass().getResourceAsStream("/solid-materials.json");
+             InputStreamReader reader = new InputStreamReader(inputStream)) {
             materials = gson.fromJson(reader, listType);
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return materials;
     }
+
 
    public List<Material> loadLiquidMaterials() {
         Gson gson = new Gson();
